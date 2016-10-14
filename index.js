d1a9072c46a345d4f9e0cbbfd1928f133f144367
ozPop <ozziepopovas@gmail.com>
2016-10-14 17:23:41 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var musicianInstruments = [];
+  for (let i = 0; i < musicians.length; i++) {
+    musicianInstruments.push(musicians[i] + " plays " + instruments[i]);
+    // console.log(musicianInstruments);
+  }
+  return musicianInstruments;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while ( i < facts.length ) {
+    facts[i] += "!!!";
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return arr;
+}
