399ada3a24338e5e9f07d728028f71a2114b20e6
Wendy Xu <guangw.xu@gmail.com>
2016-10-24 22:08:00 -0400

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts(facts) {
   var i = 0;
   while (i < facts.length) {
     facts[i] = facts[i] + "!!!";
-    ++i;
+    i++;
   }
   return facts;
 }
@@ -19,7 +19,7 @@ function iLoveTheBeatles(num) {
   var ilb = [];
   do {
     ilb.push("I love the Beatles!");
-    ++num;
+    num++;
   } while (num < 15);
   return ilb;
 }
