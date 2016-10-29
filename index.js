2dfd122a5bbe9c4f32fe764a19d45544e525bf6b
Bibi Khadiza <limakhadiza@yahoo.com>
2016-10-29 15:10:29 -0400

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function theBeatlesPlay(mus, instru){
+
+  var array = [];
+  for (let i = 0; i < mus.length; ++i)
+  { array.push(`${mus[i]} plays ${instru[i]}`);}
+  {return array;}
+}
+
+function johnLennonFacts(facts){
+  var x = 0;
+while(x < facts.length){facts[x] += "!!!";
+  x++;
+}
+return facts;
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  do { arr.push("I love the Beatles!");
+  num++;
+} while ( num < 15 );
+
+  return arr
+
+}
